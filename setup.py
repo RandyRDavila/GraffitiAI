280607bdd6440ac662b6899ddad6744510cd7a3f
Randy Davila <randyrdavila@gmail.com>
2025-01-28 17:14:08 -0600

    fixed the default version to be added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graffitiai",  # Unique name on PyPI
-    version="0.1.1",
+    version="0.1.3",
     author="Randy Davila",
     author_email="randyrdavila@gmail.com",
     description="A Python package for automated mathematical conjecturing.",
