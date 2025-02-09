f4293e9f44e544830d5854ac1b44f4542ffbf739
Randy Davila <randyrdavila@gmail.com>
2025-02-09 14:19:08 -0600

    updated version with experimental code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graffitiai",  # Unique name on PyPI
-    version="0.1.11",
+    version="0.1.12",
     author="Randy Davila",
     author_email="randyrdavila@gmail.com",
     description="A Python package for automated mathematical conjecturing.",
