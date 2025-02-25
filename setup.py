0486bd2e46f51b422490082cfae2d1a7f86be3ed
Randy Davila <randyrdavila@gmail.com>
2025-02-25 16:26:06 -0600

    updated version to 0.1.15 major change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graffitiai",  # Unique name on PyPI
-    version="0.1.14",
+    version="0.1.15",
     author="Randy Davila",
     author_email="randyrdavila@gmail.com",
     description="A Python package for automated mathematical conjecturing.",
